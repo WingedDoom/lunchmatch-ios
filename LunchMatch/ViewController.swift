7de6f840231f0db322e0a427fbb0316ec0d9ed52
Bulat Khabirov <bkhabirov@alfabank.ru>
2020-11-06 21:01:28 +0300

    Initial Commit

---
+++ LunchMatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LunchMatch
+//
+//  Created by Булат Хабиров on 06.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

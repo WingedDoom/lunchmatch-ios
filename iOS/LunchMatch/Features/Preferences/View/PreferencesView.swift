1ceb66adcdef340b75de21810afb50a3d9151948
Rozaliya <rozaliya.amirova@gmail.com>
2020-11-07 23:08:15 +0300

    added next button to preferences screen

--- iOS/LunchMatch/Features/Preferences/View/PreferencesView.swift
+++ iOS/LunchMatch/Features/Preferences/View/PreferencesView.swift
@@ -11,6 +11,8 @@ class PreferencesView: UIView {
     private lazy var tableView: UITableView = UITableView()
     private let manager = PreferencesTableViewManager()
     
+    private let button = SelectableButton()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         didLoad()
@@ -23,6 +25,7 @@ class PreferencesView: UIView {
     
     private func didLoad() {
         addSubview(tableView)
+        addSubview(button)
 
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: topAnchor, constant: 0),
@@ -30,10 +33,21 @@ class PreferencesView: UIView {
             tableView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15.0),
             tableView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15.0)
         ])
+        
+        NSLayoutConstraint.activate([
+            button.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -60.0),
+            button.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 65.0),
+            button.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -65.0)
+        ])
+
+        button.title = "Next"
+        
         tableView.translatesAutoresizingMaskIntoConstraints = false
+        button.translatesAutoresizingMaskIntoConstraints = false
         
         manager.tableView = tableView
         tableView.backgroundColor = UIColor.white
+        tableView.tableFooterView = UIView()
     }
     
     func configure(with viewModel: PreferencesViewModel) {

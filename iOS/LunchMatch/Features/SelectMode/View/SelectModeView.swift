4aae02990d4b7fba197c4ec0f6d522c5b13053fd
Bulat Khabirov <bkhabirov@alfabank.ru>
2020-11-07 19:38:31 +0300

    WIP mode selection

--- iOS/LunchMatch/Features/SelectMode/View/SelectModeView.swift
+++ iOS/LunchMatch/Features/SelectMode/View/SelectModeView.swift
@@ -5,4 +5,51 @@
 //  Created by Булат Хабиров on 07.11.2020.
 //
 
-import Foundation
+import UIKit
+
+protocol SelectModeViewDelegate: AnyObject {
+    func didSelectRoomMode()
+    func didSelectSingleMode()
+}
+
+final class SelectModeView: UIView, XibInitializable {
+    // MARK: - Properties
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    weak var delegate: SelectModeViewDelegate?
+    
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        didLoad()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        didLoad()
+    }
+    
+    private func didLoad() {
+        xibSetup()
+        
+        titleLabel.font = .appTitle1
+        descriptionLabel.font = .appBody
+    }
+    
+    func configure(with viewModel: SelectModeViewModel, animated: Bool) {
+        UIView.animate(withDuration: animated ? 0.3 : 0) {
+            self.imageView.image = viewModel.image
+            self.titleLabel.text = viewModel.title
+            self.descriptionLabel.text = viewModel.description
+        }
+    }
+    
+    @IBAction private func didSelectRoomMode() {
+        delegate?.didSelectRoomMode()
+    }
+    
+    @IBAction private func didSelectSingleMode() {
+        delegate?.didSelectSingleMode()
+    }
+}
--- iOS/LunchMatch/Features/SelectMode/ViewController/SelectModeViewController.swift
+++ iOS/LunchMatch/Features/SelectMode/ViewController/SelectModeViewController.swift
@@ -8,5 +8,46 @@
 import UIKit
 
 final class SelectModeViewController: UIViewController {
+    private var selectModeView: SelectModeView {
+        return view as! SelectModeView
+    }
     
+    override func loadView() {
+        view = SelectModeView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        selectModeView.delegate = self
+        selectModeView.configure(with: singleViewModel, animated: false)
+    }
+}
+
+extension SelectModeViewController: SelectModeViewDelegate {
+    func didSelectSingleMode() {
+        selectModeView.configure(with: singleViewModel, animated: true)
+    }
+    
+    func didSelectRoomMode() {
+        selectModeView.configure(with: roomViewModel, animated: true)
+    }
+}
+
+private extension SelectModeViewController {
+    var roomViewModel: SelectModeViewModel {
+        .init(
+            image: R.image.selectMode_SingleArt(),
+            title: R.string.localizable.selectModeRoomTitle(),
+            description: R.string.localizable.selectModeRoomDescription()
+        )
+    }
+    
+    var singleViewModel: SelectModeViewModel {
+        .init(
+            image: R.image.selectMode_SingleArt(),
+            title: R.string.localizable.selectModeSingleTitle(),
+            description: R.string.localizable.selectModeSingleDescription()
+        )
+    }
 }
